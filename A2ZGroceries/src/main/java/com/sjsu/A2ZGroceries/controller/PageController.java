5d84cd7485dd4229de924d74a76ead9556da3e86
Tejas Rane <tejas.m.rane@gmail.com>
2018-03-27 02:49:13 -0700

    Front Controller and URL mapping

---
+++ A2ZGroceries/src/main/java/com/sjsu/A2ZGroceries/controller/PageController.java
@@ -0,0 +1,15 @@
+package com.sjsu.A2ZGroceries.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	@RequestMapping(value ={"/","/home","/index"})
+	public ModelAndView index(){
+		ModelAndView mv  = new ModelAndView("page");
+		mv.addObject("greeting", "welcome to A2ZGroceries!");
+		return mv;
+	}
+}
